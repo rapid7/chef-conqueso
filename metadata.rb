186d7c12f9b5dc10db81c8ca2230351545021258
eciramella <eciramella@gmail.com>
2014-01-23 23:25:16 -0500

    Adding in all the new files generated by berkshelf

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'conqueso-chef'
+maintainer       'YOUR_COMPANY_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures conqueso-chef'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
