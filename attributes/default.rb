2d01db11ed0200fda4443abd9ba309d6851cf827
swillard-r7 <swillard@rapid7.com>
2014-04-21 15:20:22 -0400

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default['conqueso']['version'] = '0.3.7'
+default['conqueso']['version'] = '0.3.8'
 #Choose one or the other here....
 #default['conqueso']['install']['fromsource'] = true
 default['conqueso']['install']['frompackage'] = true
