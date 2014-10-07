b98b56cad1f69ddc3502e695c49d2ba95c6030cd
swillard-r7 <stephen_willard@rapid7.com>
2014-10-07 11:50:53 -0400

    Bumping conqueso version

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default['conqueso']['version'] = '0.4.1'
+default['conqueso']['version'] = '0.4.2'
 #Choose one or the other here....
 #default['conqueso']['install']['fromsource'] = true
 default['conqueso']['install']['frompackage'] = true
